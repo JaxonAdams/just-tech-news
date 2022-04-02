02523ca7d032c01797d63830c0a0a615af5759a0
Jaxon Adams <jadamsresume1@gmail.com>
2022-04-02 16:39:13 -0600

    connect to jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,19 @@
 // Import the Sequalize constructor from the library
-const Sequalize = require('sequelize');
+const Sequelize = require('sequelize');
 
 require('dotenv').config();
 
 // Create connection to the database
-const sequelize = new Sequalize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306
-});
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+    sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+        host: 'localhost',
+        dialect: 'mysql',
+        port: 3306
+    });
+}
 
 module.exports = sequelize;
