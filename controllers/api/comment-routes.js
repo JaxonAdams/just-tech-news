a791afc46ae04a873fcead4066fc26a09a880b23
Jaxon Adams <jadamsresume1@gmail.com>
2022-04-12 23:24:35 -0600

    added comment and upvote functionality

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -29,16 +29,19 @@ router.get('/', (req, res) => {
 
 // POST comment /api/comments
 router.post('/', (req, res) => {
-    Comment.create({
-        comment_text: req.body.comment_text,
-        user_id: req.body.user_id,
-        post_id: req.body.post_id
-    })
+    if (req.session) {
+        Comment.create({
+            comment_text: req.body.comment_text,
+            post_id: req.body.post_id,
+            // use the id from the session
+            user_id: req.session.user_id
+        })
         .then(dbCommentData => res.json(dbCommentData))
         .catch(err => {
             console.log(err);
             res.status(400).json(err);
         });
+    }
 });
 
 // DELETE comment /api/comments/:id
