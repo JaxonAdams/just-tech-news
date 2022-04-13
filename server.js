7a3f943729c84163587a1530bc00762b28708790
Jaxon Adams <jadamsresume1@gmail.com>
2022-04-13 13:25:35 -0600

    wrote helper functions into template

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const path = require('path');
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const helpers = require('./utils/helpers');
+const hbs = exphbs.create({ helpers });
 const session = require('express-session');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
