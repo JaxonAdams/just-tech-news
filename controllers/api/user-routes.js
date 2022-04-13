79db1741e9c9abd144610584a3e8f5d04447e25a
Jaxon Adams <jadamsresume1@gmail.com>
2022-04-12 19:36:37 -0600

    added logout functionality

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -105,7 +105,18 @@ router.post('/login', (req, res) => {
         res.json({ user: dbUserData, message: 'You are now logged in!' });
       });
     });
-  });
+});
+
+// POST /api/users/logout
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    } else {
+        res.status(404).end();
+    }
+});
 
 // PUT /api/users/1
 router.put('/:id', (req, res) => {
