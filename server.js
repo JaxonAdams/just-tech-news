ee357b869744a3e80ea6ef228730aab2c3d0cb35
Jaxon Adams <jadamsresume1@gmail.com>
2022-04-12 18:58:48 -0600

    set up session on login

--- server.js
+++ server.js
@@ -2,8 +2,23 @@ const express = require('express');
 const path = require('path');
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
+const session = require('express-session');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
+
+require('dotenv').config
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+    secret: process.env.SESSION_SECRET,
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -14,6 +29,7 @@ app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session(sess));
 
 // turn on routes
 app.use(routes);
