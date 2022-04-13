ebcb977aae77c215ca4ec8dcbfdeebf9f7baa351
Jaxon Adams <jadamsresume1@gmail.com>
2022-04-13 12:41:18 -0600

    certain sections only display if logged in

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -30,7 +30,7 @@ router.get('/', (req, res) => {
     })
     .then(dbPostData => {
         const posts = dbPostData.map(post => post.get({ plain: true })).reverse();
-        res.render('homepage', { posts });
+        res.render('homepage', { posts, loggedIn: req.session.loggedIn });
     })
     .catch(err => {
         console.log(err);
@@ -83,7 +83,7 @@ router.get('/post/:id', (req, res) => {
         const post = dbPostData.get({ plain: true });
 
         // pass data to template
-        res.render('single-post', { post });
+        res.render('single-post', { post, loggedIn: req.session.loggedIn });
     })
     .catch(err => {
         console.log(err);
